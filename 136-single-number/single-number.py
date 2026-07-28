7f8b3280053fb0138fadc0e1404eebdf2cff3189
Ankit Nagar <ankitcpu05@gmail.com>
2026-07-28 12:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (51.59%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,15 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
-
-        freq = {}
-
+        ans =0
         for i in nums:
-            if i in freq:
-                freq[i] +=1
-            else:
-                freq[i] =1
-        
-        for i in freq:
-            if freq[i] == 1:
-                return i
-        
+            ans = ans ^ i
+
+        return ans
