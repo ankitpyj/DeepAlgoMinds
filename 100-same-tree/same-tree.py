71268926b6d5ccc8084ba8307abd5874dcd715b7
Ankit Nagar <ankitcpu05@gmail.com>
2026-08-21 22:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (59.99%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -7,61 +7,19 @@
 class Solution(object):
     def isSameTree(self, p, q):
 
-        # p and q = NONE == True
-
         if p == None and q == None:
-            return True 
+            return True
 
-        # p = none ya fir q = none dono meh seh ek none == false
-        if p == None or q == None:
-            return False
-
-        #values is different == false
-
+        if p == None or q ==None:
+            return False        
+        
         if p.val != q.val:
             return False
 
-        # values is same vist left and right
         if p.val == q.val:
 
             left = self.isSameTree(p.left,q.left)
-            right = self.isSameTree(p.right,q.right)
 
-            return left and right
+            right = self.isSameTree(p.right,q.right) 
 
-
-
-
-        # outputp= []
-
-        # def prep(p):
-
-        #     if p == None:
-        #         return 
-
-        #     outputp.append(p.val)
-        #     prep(p.left)
-        #     prep(p.right)
-        
-        # prep(p)
-
-
-        # outputq = []
-
-        # def preq(q):
-
-        #     if q == None:
-        #         return 
-
-        #     outputq.append(q.val)
-        #     preq(q.left)
-        #     preq(q.right)  
-
-        # preq(q)
-
-        # return outputp == outputq     
-
-
-
-
-        
+            return left and right 
