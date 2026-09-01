9e264b68684d234eb04762ae53d9bcdd7d048d1c
Ankit Nagar <ankitcpu05@gmail.com>
2026-09-01 23:22:43 +0530

    Time: 1 ms (71.74%) | Memory: 20.2 MB (72.33%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/recover-binary-search-tree.py
@@ -1,30 +1,36 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution(object):
     def recoverTree(self, root):
-        first = [None]
-        second = [None]
-        prev = [None]
+        first = None
+        second = None
+        prev = None
 
         def inorder(root):
+            nonlocal first, second, prev
+
             if root is None:
                 return
 
             inorder(root.left)
 
-            if prev[0] and prev[0].val > root.val:
-                if first[0] is None:
-                    first[0] = prev[0]
-                second[0] = root
-
-            prev[0] = root
+            if prev and prev.val > root.val:
+                if not first:
+                    first = prev
+                second = root
+            
+            prev = root
 
             inorder(root.right)
 
         inorder(root)
 
-        first[0].val, second[0].val = second[0].val, first[0].val
+        first.val , second.val = second.val , first.val 
+
+
+
+        
