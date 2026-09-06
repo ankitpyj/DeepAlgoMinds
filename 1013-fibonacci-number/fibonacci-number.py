a154a3ff29cc3960245cfbe93ec77ea71da13f95
Ankit Nagar <ankitcpu05@gmail.com>
2026-09-06 17:23:05 +0530

    Time: 325 ms (19.68%) | Memory: 19 MB (89.00%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def fib(self, n):
+class Solution:
+    def fib(self, n: int) -> int:
 
-        dp = [0] * (n+1)
-
-        dp[0] =0
-
-        if n>=1:
-            dp[1] =1
-
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        if n == 0 :
+            return 0
         
-        return dp[n]
+        if n ==1:
+            return 1
 
+        return self.fib(n-1)+ self.fib(n-2)
 
+        
         
